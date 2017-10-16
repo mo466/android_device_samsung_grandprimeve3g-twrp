93bbfc741210a7c8248f33c04f3ed1f906e4fb14
Naim Rahman <naimgamekiller20020808@gmail.com>
2017-10-17 00:00:10 +0800

    grandprimeve3g.mk : Remove remaining prebuilt kernel commands

--- grandprimeve3g.mk
+++ grandprimeve3g.mk
@@ -1,18 +1,11 @@
 LOCAL_PATH := device/samsung/grandprimeve3g
 
-# Хакатон для билда
+# Need help for this
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab \
